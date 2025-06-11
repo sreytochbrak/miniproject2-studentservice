15e3a9017a4ea8ba2f15d59d4f7242f379d62e45
Sreytoch-Brak <137994391+sreytochbrak@users.noreply.github.com>
2025-06-11 16:13:34 +0700

    Update student-service.js

--- student-service.js
+++ student-service.js
@@ -7,4 +7,4 @@ app.post('/submitassignment', (req, res) => res.send('Assignment submitted!'));
 app.get('/viewassignment', (req, res) => res.send('Viewing assignment...'));
 app.put('/editprofile', (req, res) => res.send('Profile edited!'));
 
-app.listen(3001, () => console.log('Student service running on 3001'));
+app.listen(5000, () => console.log('Student service running on 5000'));
